fe5825ec48c15509681467bfed59637d05e2f965
Force <forceh2010@gmail.com>
2016-07-06 21:11:37 +0100

    - Created the landing page ready for API interaction to connect the user's account.

---
+++ src/Mvvm/LandingViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Windows.UI.Xaml;
+
+namespace narmail.Mvvm
+{
+    public class LandingViewModel : BindableBase
+    {
+        private bool _loginButtonEnabled = true;
+        public bool loginButtonEnabled { get { return _loginButtonEnabled; } set { SetProperty(ref _loginButtonEnabled, value); } }
+
+        private Visibility _loggingInProgressRing = Visibility.Collapsed;
+        public Visibility loggingInProgressRing { get { return _loggingInProgressRing; } set { SetProperty(ref _loggingInProgressRing, value); } }
+
+        private void toggleLoginButton(bool state = true)
+        {
+            loginButtonEnabled = state;
+            loggingInProgressRing = (state == false ? Visibility.Visible : Visibility.Collapsed);
+        }
+
+        public LandingViewModel()
+        {
+
+        }
+
+        public void connectRedditAccount()
+        {
+            // disable the button
+            toggleLoginButton(false);
+
+            
+        }
+    }
+}
---
+++ src/Views/Landing.xaml.cs
@@ -0,0 +1,20 @@
+﻿using narmail.Mvvm;
+using Windows.UI.Xaml.Controls;
+
+namespace narmail.Views
+{
+    public sealed partial class Landing : Page
+    {
+        private LandingViewModel ViewModel = null;
+        public Landing()
+        {
+            this.InitializeComponent();
+            ViewModel = (DataContext as LandingViewModel);
+        }
+
+        private void connectRedditAccount(object sender, Windows.UI.Xaml.RoutedEventArgs e)
+        {
+            ViewModel.connectRedditAccount();
+        }
+    }
+}
