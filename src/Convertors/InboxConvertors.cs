af0988954930ade8e38f844c50d72d227d0d6c47
Force <forceh2010@gmail.com>
2016-07-09 22:15:51 +0100

    - Narmail: The inbox list will now show unread messages in a different colour.

--- src/Convertors/InboxConvertors.cs
+++ src/Convertors/InboxConvertors.cs
@@ -1,6 +1,8 @@
 ﻿using narmail.Models;
 using System;
+using Windows.UI;
 using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
 
 namespace narmail.Convertors
 {
@@ -22,6 +24,25 @@ namespace narmail.Convertors
             }
             else
                 return createdTime.ToString("HH:mm");
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class MessageIsUnreadConvertor : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool isUnread = (bool)value;
+
+            // change the body colour depending on whether the message has been read or not
+            if (isUnread == true)
+                return App.currentApp.Resources["Inbox_MessageList_MessageBodyUnread"];
+            else
+                return App.currentApp.Resources["Inbox_MessageList_MessageBody"];
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, string language)
