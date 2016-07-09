6355b083356a64d2be4f471c913464b482aa8c3d
Force <forceh2010@gmail.com>
2016-07-09 14:31:29 +0100

    - Started work on the initial style of the inbox view.

---
+++ src/Controls/TabControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+
+namespace narmail.Controls
+{
+    public sealed partial class TabControl : UserControl
+    {
+        public static readonly DependencyProperty GlyphProperty = DependencyProperty.Register("Glyph", typeof(string), typeof(TabControl), null);
+
+        public string Glyph
+        {
+            get { return GetValue(GlyphProperty) as string; }
+            set { SetValue(GlyphProperty, value); }
+        }
+
+        public static readonly DependencyProperty LabelProperty = DependencyProperty.Register("Label", typeof(string), typeof(TabControl), null);
+
+        public string Label
+        {
+            get { return GetValue(LabelProperty) as string; }
+            set { SetValue(LabelProperty, value); }
+        }
+
+
+        public TabControl()
+        {
+            this.InitializeComponent();
+            this.DataContext = this;
+        }
+    }
+}
